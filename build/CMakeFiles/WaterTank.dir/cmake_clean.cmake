7ca76421a8f567c2b8d281092999cb4dd28395e8
Christopher Prainito <christopherprainito@college.harvard.edu>
2025-09-28 00:25:39 -0400

    Ignore build directory and CMake artifacts; stop tracking build/

--- build/CMakeFiles/WaterTank.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/WaterTank"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/WaterTank.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/WaterTank.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for WaterTank.
-# This may be replaced when dependencies are built.
